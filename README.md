db23b9bb889c1047381ffde17d212f5f5b460621
EC2 Default User <ec2-user@ip-172-31-31-97.ap-southeast-1.compute.internal>
2023-09-16 07:58:02 +0000

    feature: word count completed

--- README.md
+++ README.md
@@ -1,34 +1,74 @@
+# Set up
+
 ## Only first time
+
 ```bash
 wget https://dlcdn.apache.org/hadoop/common/stable/hadoop-3.3.6.tar.gz  # any version that you prefer.
 tar -xf hadoop-3.3.6.tar.gz hadoop-3.3.6/ && rm hadoop-3.3.6.tar.gz
 ```
 
 ## Start hadoop
+
 ```bash
 dc up -d
 ```
 
-## Start hadoop map reducer
-```bash
-dc run --rm wordcount bash
-cd /app/src
-javac WordCount.java -cp $(/opt/hadoop/bin/hadoop classpath)  # compile
-java WordCount
-```
-
 ## Port forwarding(local)
+
 ```bash
 # port forwarding
 ~/tools/dev_port.sh 9870 8088
 ```
 
+## Execute WordCount hadoop map reducer
+
+```bash
+dc run --rm wordcount bash
+cd /app/src
+javac -classpath $(/opt/hadoop/bin/hadoop classpath) WordCount.java  # compile
+jar -cvf WordCount.jar WordCount*.class
+/opt/hadoop/bin/hadoop jar WordCount.jar WordCount input.txt output
+```
+
+# Tips
+## REPL
+```bash
+jshell
+```
+
 # Trouble shooting
 
 ## When datanode fails because the namenode and datanode cluster ID does not match.
+
 ```bash
 docker-compose down && docker volume ls && docker volume prune
 ```
 
+# Note
+
+## How whole hadoop map reduce process works in a nutshell.(https://data-flair.training/blogs/how-hadoop-mapreduce-works/)
+1. "InputFormat" creates "InputSplits" from input data.
+  - Splits the input data into chunks(=InputSplits). e.g., text file into lines.
+2. "RecordReader" converts InputSplit into key-value pair.
+  - Reads the data chunks and converts them into key-value pairs. e.g., a line into {line number: "Hello this is a line. Hello again."}
+3. "Mapper" processes the key-value pair and generates intermediate key-value pairs.
+  - Processes the key-value pairs and generates intermediate key-value pairs. e.g., When receiving {line number: "Hello this is a line. Hello again."}, it might generate {"Hello":1}, {"Hello":1}, {"this":1}, {"is":1}, {"a":1}, {"line":1}, {"again":1} for each word.
+4. "Combiner" receives the intermediate key-value pairs and reduces a bit (With vs without Combiner: https://data-flair.training/blogs/hadoop-combiner-tutorial/)
+  - It reduces the intermediate key-value pairs. e.g., {"Hello":1}, {"Hello":1} -> {"Hello":(1,1)}
+5. "Partitioner" groups the intermediate key-value pairs from combiner by hash(key)
+  - The purpose is to send the same key-value pairs to the same reducer.
+    - So it is used when there are multiple reducers.
+    - Multiple reducers seems to be used when the data is too large to be processed by a single reducer.
+6. "Shuffling and Sorting" sends the intermediate key-value pairs to the reducer.
+  - Done by Hadoop's internal components.
+  - Network communication.
+7. "Reducer" receives the intermediate key-value pairs and generates final key-value pairs.
+  - Processes the intermediate key-value pairs and generates final key-value pairs. e.g., {"Hello":(1,1)} -> {"Hello":2}
+8. "RecordWriter" writes the final key-value pairs to the output file based on "OutputFormat".
+  - Writes the final key-value pairs to the output file. e.g., {"Hello":2} -> "Hello:2"
+
 # References
-- https://www.youtube.com/watch?v=gEm4XY04WAU
+
+- https://www.youtube.com/watch?v=gEm4XY04WAU (Hadoop MapReduce Tutorial | MapReduce Tutorial For Beginners | Hadoop Training | Edureka)
+- https://data-flair.training/blogs/how-hadoop-mapreduce-works/ (How Hadoop MapReduce Works?)
+- https://0x0fff.com/hadoop-mapreduce-comprehensive-description/ (Explains the whole hadoop map reduce process)
--- wordcount/src/WordCount.java
+++ wordcount/src/WordCount.java
@@ -11,13 +11,75 @@ import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.Reducer;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
 
 public class WordCount {
-  public static class Map extends Mapper<LongWritable, Text, Text, IntWritable> {}
-  public static class Reduce extends Reducer<Text, IntWritable, Text, IntWritable> {}
+  public static class Map extends Mapper<LongWritable, Text, Text, IntWritable> {
+    public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+      // String is built-in in Java class.
+      String line = value.toString();
+      // StringTokenizer is a class that splits a string into tokens by delimiter(whitespace in this case).
+      StringTokenizer t = new StringTokenizer(line);
+
+      while (t.hasMoreTokens()) {
+        // Allegedly, value.set is used instead of new Text for the sake of performance.
+        value.set(t.nextToken());
+
+        // *context.write
+        // The first param is the key, the second is the value.
+        // In this program where we are counting words, the key is the word itself and the value is 1 which will be
+        // used to count the number of occurrences of a word by reducer.
+        // e.g., value = "hello", context.write(value, new IntWritable(1)) will be written as "hello 1"
+        context.write(value, new IntWritable(1));
+      }
+    }
+  }
+  public static class Reduce extends Reducer<Text, IntWritable, Text, IntWritable> {
+    /**
+     * @param key     is same as the first param of context.write in map function, which was "value".
+     * @param values  is an iterable of values that was passed to context.write in map function as its second param with the same key.
+     */
+    public void reduce(Text key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
+      int sum = 0;
+      for (IntWritable x: values) {
+        sum += x.get();
+      }
+      // *context.write
+      // key is the word, value is the number of occurrences of the word.
+      // In our case, the key is equal to the first param of context.write in map function, which was "value".
+      // e.g., key = "hello", context.write(key, new IntWritable(sum)) will be written as "hello 2"
+      context.write(key, new IntWritable(sum));
+    }
+  }
+  
 
   public static void main(String[] args) throws Exception {
-    // hello world
-    System.out.println("Hello World!");
+    System.out.println("Starting WordCount program!");
+    Configuration conf = new Configuration();
+
+    Job job = Job.getInstance(conf, "WordCount");
+
+    // set the class of the main function.
+    job.setJarByClass(WordCount.class);
+    // set the class of the mapper.
+    job.setMapperClass(Map.class);
+    // set the class of the reducer.
+    job.setReducerClass(Reduce.class);
+
+    job.setOutputKeyClass(Text.class);
+    job.setOutputValueClass(IntWritable.class);
+
+    job.setInputFormatClass(TextInputFormat.class);
+    job.setOutputFormatClass(TextOutputFormat.class);
+
+    Path outputPath = new Path(args[1]);
+
+    FileInputFormat.addInputPath(job, new Path(args[0]));
+    FileOutputFormat.setOutputPath(job, new Path(args[1]));
+
+    outputPath.getFileSystem(conf).delete(outputPath, true);
+
+    System.exit(job.waitForCompletion(true) ? 0 : 1);
   }
 }
